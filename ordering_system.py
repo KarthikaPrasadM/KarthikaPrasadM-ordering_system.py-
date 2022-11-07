c90505e53c1be056d8f379d0258a9691581f412a
Karthika Prasad <104382080+KarthikaPrasadM@users.noreply.github.com>
2022-11-07 19:31:50 +0400

    Add files via upload

---
+++ ordering_system.py
@@ -0,0 +1,91 @@
+menu = {
+    1: {"name": 'espresso',
+        "price": 1.99},
+    2: {"name": 'coffee',
+        "price": 2.50},
+    3: {"name": 'cake',
+        "price": 2.79},
+    4: {"name": 'soup',
+        "price": 4.50},
+    5: {"name": 'sandwich',
+        "price": 4.99}
+}
+
+
+def calculate_subtotal(order):
+    print('Calculating bill subtotal...')
+
+    summ = 0
+    for i in order:
+        summ += i["price"]
+    sum1 = round(summ, 2)
+    return sum1
+
+
+def calculate_tax(subtotal):
+    print('Calculating tax from subtotal...')
+
+    return round(subtotal * 0.15, 2)
+
+
+def summarize_order(order):
+    print_order(order)
+
+    sub = calculate_subtotal(order)
+    tax = calculate_tax(sub)
+    tot = sub + tax
+    l = []
+    for i in order:
+        l.append(i["name"])
+    x = (l, round(tot, 2),)
+    return x
+
+
+# This function is provided for you, and will print out the items in an order
+def print_order(order):
+    print('You have ordered ' + str(len(order)) + ' items')
+    items = []
+    items = [item["name"] for item in order]
+    print(items)
+    return order
+
+
+# This function is provided for you, and will display the menu
+def display_menu():
+    print("------- Menu -------")
+    for selection in menu:
+        print(f"{selection}. {menu[selection]['name'] : <9} | {menu[selection]['price'] : >5}")
+    print()
+
+
+# This function is provided for you, and will create an order by prompting the user to select menu items
+def take_order():
+    display_menu()
+    order = []
+    count = 1
+    for i in range(3):
+        item = input('Select menu item number ' + str(count) + ' (from 1 to 5): ')
+        count += 1
+        order.append(menu[int(item)])
+    return order
+
+
+
+
+def main():
+    order = take_order()
+    print_order(order)
+
+    subtotal = calculate_subtotal(order)
+    print("Subtotal for the order is: " + str(subtotal))
+
+    tax = calculate_tax(subtotal)
+    print("Tax for the order is: " + str(tax))
+
+    items, subtotal = summarize_order(order)
+    print(items)
+    print(subtotal)
+
+
+if __name__ == "__main__":
+    main()
